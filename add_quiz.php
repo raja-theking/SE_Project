87494f100a0c887a7509b40b67ef654852f2b6e6
KunalKK99 <kunalkumawat004@outlook.com>
2019-03-08 22:30:38 +0530

    css

--- add_quiz.php
+++ add_quiz.php
@@ -23,15 +23,6 @@ $quiz_id = $_GET['quiz_id'];
             3. <input type="text" name="opt3">  <input type="checkbox" class="ll" name="ans[]" value="3"> <br>
             4. <input type="text" name="opt4">  <input type="checkbox" class="ll" name="ans[]" value="4"> <br>
             </div>
-			<div class="on"> Right Options: </div>
-	   <div class="box">
-	  <select name="answer">
-	  <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-    </select>
-	 </div>
 	 <div class="new">
     <input type="hidden" name="quiz_id" value="<?php echo $quiz_id; ?>">
             <input type="submit" value="Add"> <br>
