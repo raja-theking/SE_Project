5673ad531212d3a217e172acbcd7ef48e7033d3c
KunalKK99 <kunalkumawat004@outlook.com>
2019-03-08 21:53:39 +0530

    Working on Css

--- add_quiz.php
+++ add_quiz.php
@@ -7,23 +7,37 @@ $quiz_id = $_GET['quiz_id'];
 
 <html>
     <head>
-        <title>TODO supply a title</title>
+        <title>Online Quiz System</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<link rel="stylesheet" href="ques.css">
     </head>
-    <body>
+    <body><div class="add">
+	<h1> Please Insert The Question </h1>
         <form action="add_questions.php" method="get">
-            Question : <input type="text" name="ques"> <br>
-            Options: <br>
-            1> <input type="text" name="opt1">  <input type="checkbox" name="ans[]" value="1"> <br>
-            2> <input type="text" name="opt2">  <input type="checkbox" name="ans[]" value="2"> <br>
-            3> <input type="text" name="opt3">  <input type="checkbox" name="ans[]" value="3"> <br>
-            4> <input type="text" name="opt4">  <input type="checkbox" name="ans[]" value="4"> <br>
-
-
+ <div class="ques"> Question : <input type="text" name="ques"> <br></div>
+          <div class="op">  Options : <br></div>
+		  <div class="opt">
+            1. <input type="text" name="opt1">  <input type="checkbox" name="ans[]" value="1"> <br>
+            2. <input type="text" name="opt2">  <input type="checkbox" name="ans[]" value="2"> <br>
+            3. <input type="text" name="opt3">  <input type="checkbox" name="ans[]" value="3"> <br>
+            4. <input type="text" name="opt4">  <input type="checkbox" name="ans[]" value="4"> <br>
+            </div>
+			<div class="on"> Right Options: </div>
+	   <div class="box">
+	  <select name="answer">
+	  <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+    </select>
+	 </div>
+	 <div class="new">
     <input type="hidden" name="quiz_id" value="<?php echo $quiz_id; ?>">
             <input type="submit" value="Add"> <br>
-        </form>
-        <a href="teacher.php">DONE</a>
+      </div>
+       </form><a href="teacher.php">DONE</a>
+	   </div>
+
     </body>
 </html>
