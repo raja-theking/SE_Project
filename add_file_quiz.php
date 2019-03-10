a50568ec48b2d0545129d5c0d854154aa6e194eb
raja-theking <anuragsoni@jklu.edu.in>
2019-03-10 13:10:38 +0530

    patched add_quiz_file

--- add_file_quiz.php
+++ add_file_quiz.php
@@ -11,9 +11,24 @@ $fileo = fopen("$file","r");
 
 
 while (!feof($fileo)) {
+  $multi = 'yes';
   $contents = fgets($fileo);
   $carray = explode("|", $contents);
-  list($question, $opt1, $opt2, $opt3, $opt4, $a1, $a2, $a3, $a4, $multi) = $carray;
+  list($question, $opt1, $opt2, $opt3, $opt4, $a1, $a2, $a3, $a4) = $carray;
+
+  if($a1==0 and $a2[1]==0 and $a3[2]==0){
+    $multi = 'no';
+  }
+  if($a1==0 and $a2==0 and $a4==0){
+    $multi = 'no';
+  }
+  if($a1==0 and $a3==0 and $a4==0){
+    $multi = 'no';
+  }
+  if($a2==0 and $a3==0 and $a4==0){
+    $multi = 'no';
+  }
+
 
   $sql = "INSERT INTO quiz_questions values (NULL, '".$quiz_id."','".$question."','".$opt1."','".$opt2."','".$opt3."','".$opt4."',
     '".$a1."','".$a2."','".$a3."','".$a4."','".$multi."')";
